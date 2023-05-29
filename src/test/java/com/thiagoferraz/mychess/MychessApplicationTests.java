c428b715817f9918bc9de5722faed7e27ece584c
thiagoferrax <thiagoferrax@gmail.com>
2023-05-29 17:00:35 -0300

    Creating initial code with spring initializr.

---
+++ src/test/java/com/thiagoferraz/mychess/MychessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoferraz.mychess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MychessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
