45e90a2b60f954726d019830e0d949ff6b0ad91f
thiagoferrax <thiagoferrax@gmail.com>
2023-06-06 21:16:30 +0100

    Adding a frontend app with React.

--- src/main/java/com/thiagoferraz/mychess/repositories/BoardRepository.java
+++ src/main/java/com/thiagoferraz/mychess/repositories/BoardRepository.java
@@ -2,6 +2,8 @@ package com.thiagoferraz.mychess.repositories;
 
 import com.thiagoferraz.mychess.model.entities.Board;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardRepository extends CrudRepository<Board, Integer> {
 }
--- src/main/java/com/thiagoferraz/mychess/repositories/GameRepository.java
+++ src/main/java/com/thiagoferraz/mychess/repositories/GameRepository.java
@@ -2,6 +2,8 @@ package com.thiagoferraz.mychess.repositories;
 
 import com.thiagoferraz.mychess.model.entities.Game;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameRepository extends CrudRepository<Game, Integer> {
 }
